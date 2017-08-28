286e182e3625fdd6d0a22dc19fcb2d5732c23074
Jan Edrozo <jedrozo@gmail.com>
2017-08-28 16:10:27 -0700

    Initialized w/ letter and score hash & passed test to create new instance of score & created failing test for datatype

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -1,5 +1,18 @@
 require_relative 'spec_helper'
 
 describe "Scoring" do
+  # before do
+  #   @test_scoring = Scrabble::Scoring.new
+  # end
+
+  it 'can create an instance of scoring' do
+    letters_score = Scrabble::Scoring.new
+    letters_score.must_be_instance_of Scrabble::Scoring
+  end
+
+  it 'returns a hash of letters and their score values' do
+    letters_score = Scrabble::Scoring.new
+    letters_score.must_be_kind_of Array #should be a hash
+  end
 
 end
