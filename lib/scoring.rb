4fd6ab195051042496b10e6f14e85d7bf3ef098e
Jan Edrozo <jedrozo@gmail.com>
2017-08-30 01:21:01 -0700

    Brainstorming

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -32,21 +32,60 @@ module Scrabble
         Z: 10
       }
 
-
+    #returns the total score for the given word
     def self.score(word)
       raise ArgumentError.new("Word argument must be a string") if !word.is_a? String
+      # todo: raise error if word is not alpha
       word = word.upcase.split("")
-      score = word.map {|letter| LETTER_VALUES[letter.to_sym]}.reduce(:+)
+      score = word.map { |letter| LETTER_VALUES[letter.to_sym] }.reduce(:+)
       word.length == 7 ? score += 50 : score
     end
 
+    #returns the word in the array with the highest score
     def self.highest_score_from(array_of_words)
-      scored_words = array_of_words.map {|word| self.score(word)}
+      raise ArgumentError.new("Input must be an array!") if !array_of_words.is_a? Array
+
+      raise ArgumentError.new("Array must contain only strings!") if !array_of_words.all? {|word| word.is_a? String}
+      # "array_of_words.all? {|word| word.is_a? String}" returns either TRUE or FALSE
+      # If true, the all? method continues to the next element in the array to test.
+
+      word_score_pairs = {}
+
+      array_of_words.map do |word|
+        word_score_pairs[word] = self.score(word)
+      end
+
+      # https://stackoverflow.com/questions/6040494/how-to-find-the-key-of-the-largest-value-hash
+      max_pair = word_score_pairs.max_by{|word, value| word}
+      #max_pair will be max word => max score
+
+      return max_pair[0]
 
     end
 
   end #end of class
 
-end #end of module
 
-puts
+#Tie Breaker Ideas
+
+# max_score = 0
+# max_word = ""
+#
+# word_score_pairs.each do |word, score|
+#   # Calculate word with highest score:
+#   if score > max_score
+#     max_score = score
+#     max_word = letter
+#   end
+# end
+#
+# puts "\n\n#{word_score_pairs[max_word]} wins with a grand total of #{max_score} points!"
+#
+# #TIE BREAKER: Prints other candidates with the same vote score as the maximum vote score.
+#
+# vote_scoreer.each do |letter, score|
+#   if score == max_score && letter != max_word
+#     puts "\nOh, but look! It's a tie: #{letter_map[letter]} has the same vote score with a total of #{score} votes!\n"
+#   end
+# end
+end #end of module
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -5,7 +5,6 @@ describe "Scoring" do
 
     it 'check to see if word passed in self.score is a string' do
       word = 100004
-
       proc { Scrabble::Scoring.score(word) }.must_raise ArgumentError
     end
 
@@ -23,16 +22,42 @@ describe "Scoring" do
 
     it "a seven-tile word returns a 50 point bonus" do
       Scrabble::Scoring.score("animals").must_equal 59
+    end
+
+  end#of_self#score
+
+  describe "self#highest_score_from(array_of_words)" do
+
+    it "ensures that input is an array" do
+      array_of_words = 12
+      proc {Scrabble::Scoring.highest_score_from(array_of_words)}.must_raise ArgumentError
+    end
+
+    it "ensures elements in the inputted array are strings" do
+      array_of_words = ["apple", "banana", 12, "kiwi"]
+      proc {Scrabble::Scoring.highest_score_from(array_of_words)}.must_raise ArgumentError
+    end
+
+    it "returns output word with highest score with no ties present" do
+      array_of_words = ["apple", "banana", "kiwi"] #[9, 8, 11]
+      Scrabble::Scoring.highest_score_from(array_of_words).must_equal "kiwi"
+    end
+
+    it "tie-breaker1: the 7-lettered word wins" do
+      #if there are multiple words with same lengths pick first word in supplied list
 
     end
 
-  end
+    it "tie-breaker: the shortest word length wins" do
+      #if there are multiple words with same lengths pick first word in supplied list
 
-end
+    end
 
-describe "self#highest_score_from(array_of_words)" do
-  it "returns a string in the array" do
-    Scrabble::Scoring.highest_score_from(["animals"]).class.must_equal Array
-  end
+    it "tie-breaker: if there are multiple words with same lengths, " do
 
-end
+    end
+
+
+  end#of_self#highest_score_from
+
+end#of_Scoring_paragraph
